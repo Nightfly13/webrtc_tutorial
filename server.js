c42cc581c4a5bad4131570824c56d947cdeec72d
Nightfly13 <nightfly2013@gmail.com>
2021-08-02 13:30:14 +0200

    added insanely basic homepage

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const webSocket = new Socket({httpServer: server})
 
 let users = []
 
+app.get('/', (req,res) => {
+    res.send("AAAAAAAAAAAAAAAAH")
+})
+
 webSocket.on('request', (req)=> {
     const connection = req.accept()
 
